3c478e8638b40584701c01b1fe0349ad1ea3357c
Miguel Provencio <numeralblue@gmail.com>
2015-10-19 06:21:54 -0600

    grunt browserify

--- gruntfile.js
+++ gruntfile.js
@@ -1,11 +1,15 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks('grunt-browserify');
   grunt.initConfig({
     jshint: {
       options: {
         esnext: true
       },
       all: ["gruntfile.js","app.js","controllers/**/*.js","model/**/*.js","scripts/**/*.js","datastore/**/*.js"]
+    },
+    browserify: {
+      "public/scripts/grackle.js": ["controllers/*.js", "model/*.js", "scripts/*.js"]
     }
   });
 
