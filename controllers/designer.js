3de934c61bb2009090b487be8b7184a48135007f
Miguel Provencio <numeralblue@gmail.com>
2015-11-15 19:39:44 -0700

    highlight between items

--- controllers/designer.js
+++ controllers/designer.js
@@ -7,6 +7,8 @@ var schemaTab = require("./designerSchemaTab.html");
 import * as jsonSchema from "../model/jsonSchema";
 var visualTabEnum = Symbol();
 var schemaTabEnum = Symbol();
+const abovePlacementEnum = "above";
+const belowPlacementEnum = "below";
 
 class Designer extends Component {
 	constructor(containerID) {
@@ -93,26 +95,27 @@ class Designer extends Component {
   	var dt = event.dataTransfer;
   	dt.setData("text/field", fieldName);
 	}
+  onDragEnd(event) {
+    this.unHighlightSeperator();
+  }
   addControlListeners(editor) {
     console.log("add listeners to", editor.path);
 
     editor.container.ondragover = function (event) {
       if (event.dataTransfer.types[0] == "text/field") {
-        editor.container.style.backgroundColor = "green";
+        this.highlightSeperator(event,editor.container);
         event.preventDefault();
         event.stopPropagation();
       }
-    };
+    }.bind(this);
 
     editor.container.ondragleave = function (event) {
       if (event.dataTransfer.types[0] == "text/field") {
-        editor.container.style.backgroundColor = "";
       }
     };
 
     editor.container.ondrop = function (event) {
       if (event.dataTransfer.types[0] == "text/field") {
-        editor.container.style.backgroundColor = "";
         event.preventDefault();
         event.stopPropagation();
       }
@@ -121,6 +124,38 @@ class Designer extends Component {
     for (let key in editor.editors) {
       this.addControlListeners(editor.editors[key]);
     }
+  }
+  // calculate if placement should be above or below container
+  claculatePlacement(event,rect) {
+    let middleY = ((rect.bottom - rect.top) / 2) + rect.top;
+    if (event.clientY < middleY) {
+      return abovePlacementEnum;
+    } else {
+      return belowPlacementEnum;
+    }
+  }
+  highlightSeperator(event,container) {
+    let sep = document.getElementById("seperator");
+    let rect = container.getBoundingClientRect();
+    sep.style.height = "20px";
+    sep.style.width = rect.width + "px";
+    sep.style.backgroundColor = "gray";
+    sep.style.position = "absolute";
+    sep.style.left = rect.left + "px";
+    if (this.claculatePlacement(event, rect) === abovePlacementEnum) {
+      sep.style.top = (rect.top - 10) + "px";
+    } else {
+      sep.style.top = (rect.bottom - 10) + "px";
+    }
+  }
+  unHighlightSeperator() {
+    let sep = document.getElementById("seperator");
+    sep.style.height = "";
+    sep.style.width = "";
+    sep.style.backgroundColor = "";
+    sep.style.position = "";
+    sep.style.left = "";
+    sep.style.top = "";
   }
 }
 
