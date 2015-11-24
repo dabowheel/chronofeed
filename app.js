4c27569e5892287fa17396d8d2a4a93b787e0d42
Miguel Provencio <miguel.a.provencio@gmail.com>
2015-11-24 04:08:06 -0700

    get db connection first

--- app.js
+++ app.js
@@ -14,105 +14,107 @@ var datastore_verify = require("./datastore/verify");
 var datastore_reset = require("./datastore/reset");
 var datastore_expiringDocs = require("./datastore/expiringDocs");
 
-app.use(cookieParser(process.env.SESSION_SECRET));
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({
-    url: process.env.MONGODB_URL
-  })
-}));
+MongoClient.connect(process.env.MONGODB_URL, function (error,db) {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
 
-app.use(express.static('public'));
 
-function sendPage(req,res,next) {
-  res.sendFile(__dirname + "/public/index.html");
-}
+  app.use(cookieParser(process.env.SESSION_SECRET));
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      db: db
+    })
+  }));
 
-app.get("/blog/*", sendPage);
-app.get("/admin", sendPage);
-app.get("/profile", sendPage);
-app.get("/signup", sendPage);
-app.get("/login", sendPage);
-app.get("/verifyEmail/*", sendPage);
-app.get("/forgotPassword", sendPage);
-app.get("/forgotPasswordResult", sendPage);
-app.get("/resetPassword/*", sendPage);
-app.get("/resetPasswordResult", sendPage);
-app.get("/designer", sendPage);
+  app.use(express.static('public'));
 
-app.use("/datastore", function (req,res,next) {
-  MongoClient.connect(process.env.MONGODB_URL, function (error,db) {
-    if (error) {
-      return next(error);
-    }
+  function sendPage(req,res,next) {
+    res.sendFile(__dirname + "/public/index.html");
+  }
 
+  app.get("/blog/*", sendPage);
+  app.get("/admin", sendPage);
+  app.get("/profile", sendPage);
+  app.get("/signup", sendPage);
+  app.get("/login", sendPage);
+  app.get("/verifyEmail/*", sendPage);
+  app.get("/forgotPassword", sendPage);
+  app.get("/forgotPasswordResult", sendPage);
+  app.get("/resetPassword/*", sendPage);
+  app.get("/resetPasswordResult", sendPage);
+  app.get("/designer", sendPage);
+
+  app.use("/datastore", function (req,res,next) {
     req.db = db;
     res.set("Cache-Control", "no-cache, no-store, must-revalidate");
     res.set("Pragma", "no-cache");
     res.set("Expires", "0");
     next();
   });
-});
 
-app.get("/datastore/session",datastore_session.session);
-app.post("/datastore/signup",datastore_session.signup);
-app.post("/datastore/login",datastore_session.login);
-app.get("/datastore/logout",datastore_session.logout);
-app.post("/datastore/forgotPassword", datastore_reset.forgotPassword);
-app.post("/datastore/resetPassword", datastore_reset.resetPassword);
-app.post("/datastore/verifyEmail", datastore_verify.verifyEmail);
+  app.get("/datastore/session",datastore_session.session);
+  app.post("/datastore/signup",datastore_session.signup);
+  app.post("/datastore/login",datastore_session.login);
+  app.get("/datastore/logout",datastore_session.logout);
+  app.post("/datastore/forgotPassword", datastore_reset.forgotPassword);
+  app.post("/datastore/resetPassword", datastore_reset.resetPassword);
+  app.post("/datastore/verifyEmail", datastore_verify.verifyEmail);
 
-app.get("/datastore/Profile", datastore_users.getProfile);
-app.put("/datastore/Profile", datastore_users.saveProfile);
-app.get("/datastore/resendVerification", datastore_users.resendVerification);
+  app.get("/datastore/Profile", datastore_users.getProfile);
+  app.put("/datastore/Profile", datastore_users.saveProfile);
+  app.get("/datastore/resendVerification", datastore_users.resendVerification);
 
-app.get("/datastore/BlogList", datastore_blogs.BlogList);
+  app.get("/datastore/BlogList", datastore_blogs.BlogList);
 
-app.get("/datastore/Blog/:title", datastore_blogs.readBlog);
-app.put("/datastore/Blog", datastore_blogs.createBlog);
-app.put("/datastore/Blog/:title", datastore_blogs.saveBlog);
-app.delete("/datastore/Blog/:title", datastore_blogs.deleteBlog);
+  app.get("/datastore/Blog/:title", datastore_blogs.readBlog);
+  app.put("/datastore/Blog", datastore_blogs.createBlog);
+  app.put("/datastore/Blog/:title", datastore_blogs.saveBlog);
+  app.delete("/datastore/Blog/:title", datastore_blogs.deleteBlog);
 
-app.put("/datastore/Post", datastore_posts.createPost);
-app.put("/datastore/Post/:id", datastore_posts.updatePost);
-app.delete("/datastore/Post/:id", datastore_posts.deletePost);
+  app.put("/datastore/Post", datastore_posts.createPost);
+  app.put("/datastore/Post/:id", datastore_posts.updatePost);
+  app.delete("/datastore/Post/:id", datastore_posts.deletePost);
 
-app.get("/datastore/UserList", datastore_users.userList);
-app.delete("/datastore/User/:id", datastore_users.deleteUser);
-app.get("/datastore/ExpiredTable", datastore_expiringDocs.getExpiredTable);
-app.delete("/datastore/ExpiredReset", datastore_reset.cleanupReset);
-app.delete("/datastore/ExpiredVerify", datastore_verify.cleanupVerify);
+  app.get("/datastore/UserList", datastore_users.userList);
+  app.delete("/datastore/User/:id", datastore_users.deleteUser);
+  app.get("/datastore/ExpiredTable", datastore_expiringDocs.getExpiredTable);
+  app.delete("/datastore/ExpiredReset", datastore_reset.cleanupReset);
+  app.delete("/datastore/ExpiredVerify", datastore_verify.cleanupVerify);
 
-app.param(["title"], function (req,res,next,value) {
-  req.api = {
-    title: value
-  };
-  next();
-});
+  app.param(["title"], function (req,res,next,value) {
+    req.api = {
+      title: value
+    };
+    next();
+  });
 
-app.param(["id"], function (req,res,next,value) {
-  req.api = {
-    id: value
-  };
-  next();
-});
+  app.param(["id"], function (req,res,next,value) {
+    req.api = {
+      id: value
+    };
+    next();
+  });
 
-app.use(function(req,res,next) {
-  res.status(404).send("Not Found.");
-});
+  app.use(function(req,res,next) {
+    res.status(404).send("Not Found.");
+  });
 
-app.use(function(err,req,res,next) {
-  console.log(err.stack);
-  res.status(500).send("Application Error. " + err);
-});
+  app.use(function(err,req,res,next) {
+    console.log(err.stack);
+    res.status(500).send("Application Error. " + err);
+  });
 
-var server = app.listen(80, function () {
+  var server = app.listen(80, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 
+  });
 });
